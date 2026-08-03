51a9b3c52cdf77e1f2e3d5eef30fd411b779fae8
Okoh Miracle <myracleokoh@gmail.com>
2026-08-03 10:07:30 +0100

    First Python program

---
+++ lesson01_hello.py
@@ -0,0 +1,7 @@
+print("Hello, world!")
+
+print("My name is Miracle.")
+
+print("Today is the first day of my Computer Science journey.")
+
+print("One day, I'll build AI systems that change lives.")
